b02f83b3c7c4836c15dda8b8329985b65d8fd842
Naomi Quinones <52065567+naomiquinones@users.noreply.github.com>
2026-01-28 19:52:49 +0000

    Add lab1 file

---
+++ lab1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int points = 14;
+    string name = "Naomi";
+
+    cout << "Hello, " << name << "! The computer has seen fit to assign you " << points << " points." << endl;
+    
+    return 0;
+}
